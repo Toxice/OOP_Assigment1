8e9f482fa219593f1143149011e0c5e2e3a2bc0c
Mor <toxice290@gmail.com>
2025-11-10 16:34:49 +0200

    Better

--- src/GUI/Lines/AbstractArtDrawing.java
+++ src/GUI/Lines/AbstractArtDrawing.java
@@ -1,6 +1,7 @@
 package GUI.Lines;
 
 import Basics.Line;
+import Basics.Point;
 import biuoop.DrawSurface;
 import biuoop.GUI;
 
@@ -21,41 +22,21 @@ public class AbstractArtDrawing {
      * Main Function
      */
     static void main() {
-        ArrayList<Line> lines = getLines();
+        ArrayList<Line> lines = setLines();
+        ArrayList<Point> MiddlePoints = getMiddlePoints(lines);
+        ArrayList<Point> IntersectionPoints = getIntersectionPoints(lines);
         GUI gui = new GUI(Title, width, height);
         DrawSurface drawSurface = gui.getDrawSurface();
-        for (int i = 0; i < 10; ++i) {
-            // Lines Drawing Loop
-            drawLine(lines.get(i), drawSurface);
-            drawMiddlePoint(lines.get(i), drawSurface);
-        }
-        // Intersection Points Loop
-        for (int i = 0; i < 10; ++i) {
-            for (int j = 0; j < 10; ++j) {
-                if (i == j) continue;
-                if (lines.get(i).intersectionWith(lines.get(j)) != null) {
-                    drawSurface.setColor(Color.RED);
-                    drawSurface.fillCircle((int) lines.get(i).intersectionWith(lines.get(j))
-                            .getX(),(int) lines.get(i).intersectionWith(lines.get(j))
-                            .getY(), 4);
-                    System.out.println(lines.get(i).intersectionWith(lines.get(j)));
-                }
-            }
-        }
-        gui.show(drawSurface);
-    }
+        drawLines(lines, drawSurface);
+        drawMiddlePoints(MiddlePoints, drawSurface);
+        drawIntersectionPoints(IntersectionPoints, drawSurface);
 
-    public static ArrayList<Line> getLines() {
-        ArrayList<Line> lines = new ArrayList<>(10);
-        for (int i = 0; i < 10; ++i) {
-            lines.add(createLine(random.nextInt(width / 2) + 1, random.nextInt(height / 2) + 1,
-                    random.nextInt(width / 2) + 1, random.nextInt(height / 2) + 1));
-        }
-        return lines;
+        gui.show(drawSurface);
     }
 
     /**
      * Creates a Line from x,y coordinates
+     *
      * @param x1: coordinate
      * @param y1: coordinate
      * @param x2: coordinate
@@ -66,41 +47,72 @@ public class AbstractArtDrawing {
         return new Line(x1, y1, x2, y2);
     }
 
-    /**
-     * Draws the Line to the DrawSurface
-     * @param line: Line Object
-     * @param drawSurface: DrawSurface Object
-     */
-    public static void drawLine(Line line, DrawSurface drawSurface) {
-        drawSurface.drawLine((int) line.getStart().getX(), (int) line.getStart().getY(),
-                (int) line.getEnd().getX(), (int) line.getEnd().getY());
-    }
-
-    /**
-     * Draws the Middle Point of a Line
-     * @param line: Line Object
-     * @param drawSurface: DrawSurface Object
-     */
-    public static void drawMiddlePoint(Line line, DrawSurface drawSurface) {
-        drawSurface.setColor(Color.BLUE);
-        drawSurface.fillCircle((int) Math.round(line.middle().getX()), (int) Math.round(line.middle().getY()), 3);
-    }
-
-    /**
-     * Draw the Lines
-     * @param lines: ArrayList of Lines
-     * @param drawSurface: DrawSurface Object
-     */
     public static void drawLines(ArrayList<Line> lines, DrawSurface drawSurface) {
         drawSurface.setColor(Color.BLACK);
+        for (Line line : lines) {
+            drawSurface.drawLine((int)Math.round(line.getStart().getX()), (int)Math.round(line.getStart().getY()), (int)Math.round(line.getEnd().getX())
+                    ,(int)Math.round(line.getEnd().getY()));
+        }
+    }
+
+    /**
+     * Draws the Intersection Point of two Lines
+     *
+     * @param intersectionPoints: Point ArrayList
+     * @param drawSurface: DrawSurface Object
+     */
+    public static void drawIntersectionPoints(ArrayList<Point> intersectionPoints, DrawSurface drawSurface) {
+        drawSurface.setColor(Color.RED);
+        for (Point intersectionPoint : intersectionPoints) {
+            drawSurface.fillCircle((int)Math.round(intersectionPoint.getX()), (int)Math.round(intersectionPoint.getY()), 3);
+        }
+    }
+
+    public static void drawMiddlePoints(ArrayList<Point> middlePoints, DrawSurface drawSurface) {
+        drawSurface.setColor(Color.BLUE);
+        for (Point middle : middlePoints) {
+            drawSurface.fillCircle((int)Math.round(middle.getX()), (int)Math.round(middle.getY()), 3);
+        }
+    }
+
+    public static ArrayList<Line> setLines() {
+        ArrayList<Line> lines = new ArrayList<>();
+        for (int i = 0; i < 10; ++i) {
+                lines.add(createLine(random.nextInt(width / 2) + 1, random.nextInt(height / 2) + 1,
+                        random.nextInt(width / 2) + 1, random.nextInt(height / 2) + 1));
+        }
+        return lines;
+    }
+
+    public static ArrayList<Point> getMiddlePoints(ArrayList<Line> lines) {
+        ArrayList<Point> middlePoints = new ArrayList<>(lines.size());
+        for (Line line : lines) {
+            middlePoints.add(line.middle());
+        }
+        return middlePoints;
+    }
+
+    public static boolean isIntersects(Line line, Line other) {
+        return line.isIntersecting(other) && (line.intersectionWith(other) != null);
+    }
+
+    public static Point getIntersectionPoint(Line line, Line other) {
+        return line.intersectionWith(other);
+    }
+
+    public static ArrayList<Point> getIntersectionPoints(ArrayList<Line> lines) {
+        ArrayList<Point> intersectionPoints = new ArrayList<>();
         for (int i = 0; i < lines.size(); ++i) {
             for (int j = 0; j < lines.size(); ++j) {
                 if (i == j) continue;
-                drawLine(lines.get(i), drawSurface);
-                drawMiddlePoint(lines.get(i), drawSurface);
+                if (isIntersects(lines.get(i), lines.get(j))) {
+                    intersectionPoints.add(getIntersectionPoint(lines.get(i), lines.get(j)));
+                }
             }
         }
+        return intersectionPoints;
     }
+
 }
 
 
